51e138a38a31ac03bf3373697a09ea30735f5809
Anushk Jain <anushk2201@gmail.com>
2023-12-21 10:37:43 +0530

    feat: handled GET request for tours

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
 const express = require('express');
+const fs = require('fs');
 
 // Here, the express is a function that we get from the express module which upon on calling will add a bunch of methods to our app variable here.
 const app = express();
 
 // Defining routes
 // The way is simple app.<http-method>(<url>, <callback-function-to-define-what-to-do-now>)
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
 
-    // Here, the status method will set the response status and the send method will send the string response to the client
-    // res.status(200).send('Hello from the server side');
+//     // Here, the status method will set the response status and the send method will send the string response to the client
+//     // res.status(200).send('Hello from the server side');
 
-    // Here, we are using the json method to send the json response to the client
-    // By using the json method, it will automatically set our content type to application/json
-    res.status(200).json({
-        message: "Hello from the server side!!",
-        app: 'Natours'
-    });
-});
+//     // Here, we are using the json method to send the json response to the client
+//     // By using the json method, it will automatically set our content type to application/json
+//     res.status(200).json({
+//         message: "Hello from the server side!!",
+//         app: 'Natours'
+//     });
+// });
 
 // Here, we are just creating the post request for the same url
-app.post('/', (req, res) => {
-    res.status(200).send('You can post to this endpoint')
-})
+// app.post('/', (req, res) => {
+//     res.status(200).send('You can post to this endpoint')
+// })
+
+// Here, we are reading the data from a file and parsing it to JSON
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+
+        // According to JSend JSON Formatting
+        status: 'success',
+        results: tours.length,
+        data: {
+            
+            // tours: tours -- As both the name of the key and value are same, we can just write 
+            tours
+        }
+    });
+});
 
 // The first method that we will use is basically listen method to start up the server.
 const port = 3000;
