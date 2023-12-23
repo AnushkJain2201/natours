b5b5d69b31fac746756b386c2d340425485d9561
Anushk Jain <anushk2201@gmail.com>
2023-12-23 20:30:53 +0530

    feat: defined route for users

--- app.js
+++ app.js
@@ -117,6 +117,41 @@ const deleteTour = (req, res) => {
     })
 }
 
+const getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This Route is not yet defined'
+    })
+}
+
+const createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This Route is not yet defined'
+    })
+}
+
+const getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This Route is not yet defined'
+    })
+}
+
+const updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This Route is not yet defined'
+    })
+}
+
+const deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This Route is not yet defined'
+    })
+}
+
 // app.get('/api/v1/tours', getAllTours);
 
 // app.post('/api/v1/tours', createTour);
@@ -130,6 +165,10 @@ const deleteTour = (req, res) => {
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 const port = 3000;
 app.listen(port, () => {
