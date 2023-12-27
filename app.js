1f9e38159431069c9cb223d368a3868150975052
Anushk Jain <anushk2201@gmail.com>
2023-12-27 15:10:40 +0530

    feat: served static files

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
     console.log("Hello from the middleware");
 
