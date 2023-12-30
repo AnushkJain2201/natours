d79ef6f6f20710943c3409236747caaeca6fed33
Anushk Jain <anushk2201@gmail.com>
2023-12-30 20:02:54 +0530

    feat: created a simple tour model

--- server.js
+++ server.js
@@ -19,6 +19,24 @@ mongoose.connect(DB).then(() => {
     console.log("DB Connection is successful!!");
 })
 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
