4c6e4d7656bb964f81a6da3dd661d907a9eb2d8a
Anushk Jain <anushk2201@gmail.com>
2023-12-30 19:20:44 +0530

    feat: connected mongodb with mongoose

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
-
 const app = require('./app');
 
- 
+dotenv.config({path: './config.env'});
+
+// To change the template passwrod with the original password
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// Used for older version
+// mongoose.connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+// }).then(() => {
+//     console.log("DB Connection is successful!!");
+// })
+
+mongoose.connect(DB).then(() => {
+    console.log("DB Connection is successful!!");
+})
+
 const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
