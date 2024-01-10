6d96cf00c46e5574e59f7fd2445bafc3820197f6
Anushk Jain <anushk2201@gmail.com>
2024-01-10 19:46:03 +0530

    feat: added built in validators.

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,9 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-        trim: true
+        trim: true,
+        maxLength: [40, "A tour name must have less or equal than 40 characters"],
+        minLength: [10, "A tour name must have more or equal than 10 characters"]
     },
 
     duration: {
@@ -21,12 +23,18 @@ const tourSchema = new mongoose.Schema({
 
     difficulty: {
         type: String, 
-        require: [true, "A tour must have a difficulty"]
+        require: [true, "A tour must have a difficulty"],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either easy, medium, difficult'
+        }
     },
 
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, "Rating must be below 5.0"]
     },
 
     ratingsQuantity: {
