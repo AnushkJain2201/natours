4ec441e693ecd70aca1a7fca714ebf036e1c8d5f
Anushk Jain <anushk2201@gmail.com>
2024-01-12 15:31:42 +0530

    feat: global error handling middleware added.

--- app.js
+++ app.js
@@ -34,11 +34,28 @@ app.use('/api/v1/users', userRouter);
 // The app.all will run for routes for all the http methods like get post patch delete etc;
 // The * in the url means all the route that is unhandled
 app.all("*" , (req, res, next) => {
-    res.status(404).json({
-        status: 'failed',
+    // res.status(404).json({
+    //     status: 'failed',
 
-        // The originalUrl is the property of req that contains the url fron which we send the request.
-        message: `Can't find ${req.originalUrl} on this server`
+    //     // The originalUrl is the property of req that contains the url fron which we send the request.
+    //     message: `Can't find ${req.originalUrl} on this server`
+    // })
+
+    const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    // If we pass err in the next function the express will understand that it is an error and then it will skip all the middleware and lands directly into the error middleware
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
     })
 });
 
