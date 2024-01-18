4342e87fa3cc5c1f994461840242ae35218c49cb
Anushk Jain <anushk2201@gmail.com>
2024-01-18 20:36:50 +0530

    feat: added better errors and refactoring.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const morgan = require('morgan');
 
+const AppError = require("./utils/appError");
+const globalError = require('./controllers/errorController');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes')
 
@@ -41,22 +44,28 @@ app.all("*" , (req, res, next) => {
     //     message: `Can't find ${req.originalUrl} on this server`
     // })
 
-    const err = new Error(`Can't find ${req.originalUrl} on this server`);
-    err.status = 'fail';
-    err.statusCode = 404;
+    // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    // err.status = 'fail';
+    // err.statusCode = 404;
+
+
 
     // If we pass err in the next function the express will understand that it is an error and then it will skip all the middleware and lands directly into the error middleware
-    next(err);
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
+// // A global error handlin middleware
+// app.use((err, req, res, next) => {
+//     err.statusCode = err.statusCode || 500;
+//     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
-});
+//     res.status(err.statusCode).json({
+//         status: err.status,
+//         message: err.message
+//     })
+// });
+
+// global error middleware
+app.use(globalError);
 
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,10 @@
+// A global error handlin middleware
+module.exports =  ((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    })
+});
