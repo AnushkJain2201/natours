92456e1d562695dc5a93519c44d5c78502754aba
Anushk Jain <anushk2201@gmail.com>
2024-01-24 21:58:10 +0530

    feat: created new user using the signup controller.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
-const {getAllUsers, createUser, getUser, updateUser, deleteUser} = userController;
+const { getAllUsers, createUser, getUser, updateUser, deleteUser } = userController;
+const { signup } = authController;
+
+
 const router = express.Router();
+
+router.post('/signup', signup);
 
 router.route('/').get(getAllUsers).post(createUser);
 
