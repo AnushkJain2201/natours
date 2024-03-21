7ca8956f691b6c49efe5f381d86acb191f587c94
Anushk Jain <anushk2201@gmail.com>
2024-03-21 22:18:15 +0530

    feat: implemented basic nested route.

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewController = require('./../controllers/reviewController');
 
 // destructuring to get all the exported handlers
 const { getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours, getTourStats, getMonthlyPlan } = tourController;
 const { protect, restrictTo } = authController;
+const {createReview} = reviewController;
 
 const router = express.Router();
 
@@ -17,5 +19,7 @@ router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/').get(protect, getAllTours).post(createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
+
+router.route('/:tourId/reviews').post(protect, restrictTo('user'), createReview);
 
 module.exports = router;
