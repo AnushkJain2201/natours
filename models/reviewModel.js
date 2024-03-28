38c401fb1fdc341fe86d4890d402aab0258e17d1
Anushk Jain <anushk2201@gmail.com>
2024-03-28 20:42:09 +0530

    feat: calculated the average rating on tours.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -17,13 +18,13 @@ const reviewSchema = new mongoose.Schema({
         default: Date.now()
     },
 
-    tour:{
+    tour: {
         type: mongoose.Schema.ObjectId,
         ref: "Tour",
         required: [true, "Review must belong to a tour"]
     },
 
-    user:{
+    user: {
         type: mongoose.Schema.ObjectId,
         ref: "user",
         required: [true, "Review must belong to a user"]
@@ -33,7 +34,7 @@ const reviewSchema = new mongoose.Schema({
     toObject: { virtuals: true }
 });
 
-reviewSchema.pre(/^find/, function(next) {
+reviewSchema.pre(/^find/, function (next) {
     // this.populate({
     //     path: "tour",
     //     select: "name"
@@ -48,6 +49,57 @@ reviewSchema.pre(/^find/, function(next) {
         select: 'name photo'
     })
     next();
+});
+
+// A static method to calculate average ratings
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+
+    // In the static method this point to the current model
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRatings: { $sum: 1 },
+                avgRating: {$avg: '$rating'},
+            }
+        }
+    ]);
+
+    if(stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRatings,
+            ratingsAverage: stats[0].avgRating
+        })
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5
+        })
+    }
+    
+}
+
+reviewSchema.post('save', function() {
+
+    // Here we are using this.constructor instead of the Review because Review is not defined till this line of code
+    this.constructor.calcAverageRatings(this.tour);
+
+})
+
+// Here we are creating a pre middleware to calAverageRatings in case we are updating or deleting any review
+// As we have have used findByIdAndDelete and findByIdAndUpdate methods in order to delete and update review but we are using findOneAndUPdate and findOneAndDelete as we know these methods uses the same internally
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+    // Here this will give the current query, but we want the review doc on which it is operating on. So, heres the trick
+    this.r = await this.clone().findOne();    
+    next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function() {
+    await this.r.constructor.calcAverageRatings(this.r.tour);
+    
 });
 
 
