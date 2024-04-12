6929299e112bcd0f463a45b65244d9c9406679c6
Anushk Jain <anushk2201@gmail.com>
2024-04-12 22:07:15 +0530

    feat: used the extend feature of pug templates.

--- app.js
+++ app.js
@@ -81,7 +81,19 @@ app.get('/', (req, res) => {
         tour: 'The Forest Hiker',
         user: "Anushk"
     });
-})
+});
+
+app.get('/overview', (req, res) => {
+    res.status(200).render('overview', {
+        title: 'All tours'
+    })
+});
+
+app.get('/tour', (req, res) => {
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker Tour'
+    })
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
