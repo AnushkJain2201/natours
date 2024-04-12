678a82c7f4892cc72fb56d6b6b69f6d9d135affc
Anushk Jain <anushk2201@gmail.com>
2024-04-12 22:59:27 +0530

    completed: looping in the pug template.

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+exports.getOverview = catchAsync(async (req, res, next) => {
+    // Get the tour data from the collection
+    const tours = await Tour.find();
+
+    // build The template
+
+    // render the template using the data from 1
     res.status(200).render('overview', {
-        title: 'All tours'
+        title: 'All Tours',
+        tours
     });
-}
+})
 
 exports.getTour = (req, res) => {
     res.status(200).render('tour', {
