6253252fb789a8b38c0d941efc82fde590f4a192
Anushk Jain <anushk2201@gmail.com>
2024-06-21 20:46:31 +0530

    feat: created a complex email handler.

--- utils/email.js
+++ utils/email.js
@@ -1,36 +1,73 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('htmltotext');
 
-const sendEmail = async options => {
-    // 1) Create a transporter
-    // Transporter is a service that actually sends the email
-    // Now, we are going to use a special development service, which basically fakes to send emails to real addresses. But, in reality, these emails end up trapped in a development inbox, so that we can then take a look at how they look later in production. So, that service is called Mailtrap, let's now signup for that
-    const transporter = nodemailer.createTransport({
-
-        // Uncommment out service if using outlook
-        // service: 'outlook',
-        
-        // Comment host and port if using outlook
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        }
-
-        // Activate in email 'less secure app" option if using email
-    })
-
-    // 2) Define the email options
-    const mailOptions = {
-        from: 'Anushk Jain <anushk2201@gmail.com>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-        // html:
+// sending welcome email will be done by the sendWelcome() function. Similarly we will create different functions for sending email at different instances.
+// new Email(user, url).sendWelcome();
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstName = user.name.split(' ')[0];
+        this.utl = url;
+        this.from = `Anushk Jain <${process.env.EMAIL_FROM}>`
     }
 
-    // 3) Actually sends the email
-    await transporter.sendMail(mailOptions);
+    // This will create a new transporter as per the environment
+    newTransport() {
+        // When we are in production mode, we will send actual email using sendgrid, but in development we will use the mailtrap service for testing of sending the email.
+
+        if(process.env.NODE_ENV === 'production') {
+            // Send using the sendgrid services
+            return 1;
+        }
+
+        // If not in production, send it using the nodemailer with the mailtrap service
+        return nodemailer.createTransport({
+
+            // Uncommment out service if using outlook
+            // service: 'outlook',
+            
+            // Comment host and port if using outlook
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+    
+            // Activate in email 'less secure app" option if using email
+        })
+    }
+
+    // This function will actual send the email
+    async send(template, subject) {
+        // 1) Render HTML based on the pug template
+        // this renderFile() will create the html out of the pug template
+        const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        });
+
+        // 2) Define the email options
+        // We are defining both text an html versions as some people prefers text instead of html
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: htmlToText.fromString(html)
+        };
+
+        // 3) Create a transporter and send mail
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    // this will send the welcome email
+    async sendWelcome() {
+        // the template will be a pug template
+        await this.send('welcome', 'Welcome to the Natours Family!');
+    }
+
 }
 
-module.exports = sendEmail;
