9048536ec2e4137db578d1ced60504dd0b5ee777
Anushk Jain <anushk2201@gmail.com>
2024-06-22 19:21:35 +0530

    feat: email can now be send in production too.

--- utils/email.js
+++ utils/email.js
@@ -3,6 +3,7 @@ const pug = require('pug');
 const htmlToText = require('html-to-text');
 // sending welcome email will be done by the sendWelcome() function. Similarly we will create different functions for sending email at different instances.
 // new Email(user, url).sendWelcome();
+console.log(process.env.NODE_ENV === 'production');
 module.exports = class Email {
     constructor(user, url) {
         this.to = user.email;
@@ -15,17 +16,33 @@ module.exports = class Email {
     newTransport() {
         // When we are in production mode, we will send actual email using sendgrid, but in development we will use the mailtrap service for testing of sending the email.
 
-        if(process.env.NODE_ENV === 'production') {
+        if (process.env.NODE_ENV === 'production') {
             // Send using the sendgrid services
-            return 1;
+            console.log("i ma here n oehngtoewh aew sdnfo hnweo ");
+            return nodemailer.createTransport({
+                service: "outlook",
+                host: "smtp-mail.outlook.com",
+                port: 587,
+                secure: false,
+
+                // host: process.env.BREVO_HOST,
+                // port: process.env.BREVO_PORT,
+                // auth: {
+                //     user: process.env.BREVO_USERNAME,
+                //     pass: process.env.BREVO_PASSWORD
+                // }
+                auth: {
+                    user: process.env.OUTLOOK_USER,
+                    pass: process.env.OUTLOOK_PASSWORD
+                }
+            });
         }
 
         // If not in production, send it using the nodemailer with the mailtrap service
         return nodemailer.createTransport({
-
             // Uncommment out service if using outlook
             // service: 'outlook',
-            
+
             // Comment host and port if using outlook
             host: process.env.EMAIL_HOST,
             port: process.env.EMAIL_PORT,
@@ -33,7 +50,7 @@ module.exports = class Email {
                 user: process.env.EMAIL_USERNAME,
                 pass: process.env.EMAIL_PASSWORD
             }
-    
+
             // Activate in email 'less secure app" option if using email
         })
     }
